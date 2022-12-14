e84f20057c060be1fc6610526aaa6f6e8c51e953
Geronimo321 <76615426+Geronimo321@users.noreply.github.com>
2022-12-14 15:25:43 -0600

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,6 @@
 public class Calculate {
     
-    public void main(String[] args){
+    public static void main(String[] args){
         int i =0;
         while(i < 100){
             System.out.println(i);
