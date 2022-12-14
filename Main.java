1d66edfc92e22093ac454524f0661c8b5610d639
Geronimo321 <76615426+Geronimo321@users.noreply.github.com>
2022-12-14 14:33:15 -0600

    prints 100 digits of fibonacci sequence

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int count = 100;
+        System.out.print(0 +" "+ 1);
+        printFib(count-1);
     }
+    static float n1=0;
+    static float n2=1;
+    static float n3=0;
+    static void printFib(int n){
+        if(n >0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.print(" "+n3);
+            printFib(n-1);
+        }
+    }
+
 }
