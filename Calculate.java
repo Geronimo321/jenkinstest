58dd54fa5dbc9805745c3c133c92ffb55ee715e3
Geronimo321 <76615426+Geronimo321@users.noreply.github.com>
2022-12-14 15:24:36 -0600

    added main

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,10 @@
 public class Calculate {
-    Calculate(){
+    
+    public void main(String[] args){
         int i =0;
         while(i < 100){
             System.out.println(i);
             i++;
-        }
+        } 
     }
 }
